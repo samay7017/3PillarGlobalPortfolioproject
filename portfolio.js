929d8737349ba3757bea1f260c986a530b915f8c
samay7017 <98939122+samay7017@users.noreply.github.com>
2022-06-17 21:23:49 +0530

    portfolio project added

---
+++ portfolio.js
@@ -0,0 +1,48 @@
+let highlightBox = document.querySelector('.highlight-box');
+
+let titles = [
+    ['🌱', 'Educator'],
+    ['💻', 'Programmer'],
+    ['🌐', 'Developer'],
+    ['🔥', 'Motivator'],
+    ['📔', 'Content Creator']
+]
+
+let i = 0;
+
+setInterval(function(){
+    setTimeout(function(y){
+        highlightBox.innerHTML = '';
+        let a = document.createElement('span');
+        let b = document.createElement('div');
+        let c = document.createElement('span');
+        
+        a.innerText = titles[y][0];
+        a.classList.add('highlight1');
+        b.classList.add('highlight2');
+        c.innerText = titles[y][1];
+        
+        b.appendChild(c);
+        highlightBox.appendChild(a);
+        highlightBox.appendChild(b);
+        i++;
+        if(i >= 5){
+            i = 0;
+        }
+    },0,i)
+},1500)
+let j=0;
+let colorbox = document.querySelector('.colortext');
+let techs = ['ReactJs' ,'Python' , 'Django' ,'Javascript'];
+setInterval(function(){
+    setTimeout(function(){
+
+        colorbox.innerText = techs[j];
+        colorbox.style.color= "#"+Math.random().toString(16).substring(2,8);
+        j++;
+        if(j>3){
+            j=0;
+        }
+      
+    },1)
+},1500)
